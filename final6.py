8fff985a77589cdba073e47ebfdaac98e886ec83
nafnaf <nafpigos@gmail.com>
2017-10-18 23:26:43 +0300

    Update final6.py

--- final6.py
+++ final6.py
@@ -4,6 +4,17 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup as soup
 from time import gmtime, strftime
 from tabulate import tabulate
+import os
+
+TOKEN = "465434046:AAHZLWGbW2WfCmx1AWi6sfjOMm_tNVrMkW4"
+PORT = int(os.environ.get('PORT', '5000'))
+updater = Updater(TOKEN)
+# add handlers
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://meteokavgr.herokuapp.com/" + TOKEN)
+updater.idle()
 #BeautifulSoup 
 def scrap():
     url = 'http://www.meteokav.gr/weather/'
