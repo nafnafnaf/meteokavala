afb85be775d2480d1c0189ad9f6ae8ed2e8468b9
dimi nafpigos <dimi@dimi-ThinkPad-X61s.lan>
2017-10-20 14:23:53 +0300

    new lib

--- final6.py
+++ final6.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup as soup
 from time import gmtime, strftime
 from tabulate import tabulate
-from telegram.ext import Updater
+
 TOKEN= os.environ['TELEGRAM_TOKEN']
 #some_api_token = os.environ['SOME_API_TOKEN']
 
@@ -46,19 +46,60 @@ def scrap():
      ]
     return tabulate(values_list)
 
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(msg, content_type, chat_type, chat_id, strftime("%a, %d %b %Y %H:%M:%S +0000"))
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 
-    if content_type == 'text':
-        bot.sendMessage(chat_id, scrap())
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle).run_as_thread()
-print ('Listening ...')
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    update.message.reply_text('Hi!')
 
-# Keep the program running.
-while 1:
-    time.sleep(1)
+
+def help(bot, update):
+    update.message.reply_text('Help!')
+
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("TOKEN")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
 
